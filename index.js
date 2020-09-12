b68f3b79f91f6db73b3421381668127faab0bc15
SEUNGYONG KIM <sseungyong@gmail.com>
2020-09-12 13:42:09 +0900

    fix server error

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import { GraphQLServer } from "graphql-yoga";
 import resolvers from "./graphql/resolvers";
+import cors from "cors";
 
 const server = new GraphQLServer({
   typeDefs: "graphql/schema.graphql",
   resolvers,
+  // cors: {
+  //   origin: ["http://localhost:3000"],
+  //   credentials: true,
+  // },
 });
+
+// server.use(cors());
+
+// const opts = {
+//   port: 4000,
+//   cors: {
+//     credentials: true,
+//     origin: ["http:://localhost:3000"],
+//   },
+// };
 
 server.start(() => console.log("GraphQL Server Running"));
